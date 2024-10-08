d50e47e189a00291905bf48e43077b7e55a1e64f
voffie <viktorsoderborg@gmail.com>
2024-10-08 22:18:58 +0200

    feat: first draft of plugins dir

--- lua/plugins/color_scheme.lua
+++ lua/plugins/color_scheme.lua
@@ -1,8 +1,29 @@
 return {
   {
     "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      require("catppuccin").setup({})
+      require("catppuccin").setup({
+        integrations = {
+          cmp = true,
+          fidget = true,
+          gitsigns = true,
+          indent_blankline = {
+            enabled = false,
+            scope_color = "sapphire",
+            colored_indent_levels = false,
+          },
+          mason = true,
+          native_lsp = { enabled = true },
+          noice = true,
+          notify = true,
+          symbols_outline = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+        },
+      })
 
       vim.cmd.colorscheme("catppuccin-macchiato")
     end,
